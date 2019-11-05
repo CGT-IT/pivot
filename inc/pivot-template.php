5130ffd3f1120c5cd203d48c2476bcc0cde19113
mdegembe <maximedeg@gmail.com>
2019-11-05 10:00:19 +0100

    add pre_handle_404 filter to avoid status 404 on pivot page

--- inc/pivot-template.php
+++ inc/pivot-template.php
@@ -189,7 +189,91 @@ function pivot_add_rewrite_rules() {
   }
 }
 add_action('init', 'pivot_add_rewrite_rules');
- 
+
+// Check if we are in custom page, to override bypass 404 status
+add_filter('pre_handle_404', function($preempt, $wp_query) {
+  global $wp;
+  $customPages = pivot_get_pages_path();
+  $customPages[] = array('path' => 'details');
+  if ($key=array_search($wp->request, array_column($customPages, 'path'))) {
+    pivot_create_fake_post($customPages[$key]['title'], $customPages[$key]['path']);
+    $preempt = true;
+  }
+
+  return $preempt;
+}, 10, 2);
+
+/**
+ * Create a fake post
+ * @global Object $wp_query
+ * @global Object $wp
+ * @param string $title
+ * @param string $path
+ */
+function pivot_create_fake_post($title, $path){
+  global $wp_query;
+  global $wp;
+  // negative ID, to avoid clash with a valid post
+  $post_id = -99;
+  
+  $post = new stdClass();
+  $post->ID = $post_id;
+  $post->post_author = 1;
+  $post->post_date = current_time( 'mysql' );
+  $post->post_date_gmt = current_time( 'mysql', 1 );
+  $post->post_title = $title;
+  $post->post_content = '';
+  $post->post_status = 'publish';
+  $post->comment_status = 'closed';
+  $post->ping_status = 'closed';
+  // append random number to avoid clash
+  $post->post_name = $path; 
+  $post->post_type = 'page';
+  // important!
+  $post->filter = 'raw';
+  
+  $wp_post = new WP_Post( $post );
+  wp_cache_add($post_id, $wp_post, 'posts');
+  
+  // Update the main query
+  $wp_query->post = $wp_post;
+  $wp_query->posts = array($wp_post);
+  $wp_query->queried_object = $wp_post;
+  $wp_query->queried_object_id = $post_id;
+  $wp_query->found_posts = 1;
+  $wp_query->post_count = 1;
+  $wp_query->max_num_pages = 1; 
+  $wp_query->is_page = true;
+  $wp_query->is_singular = true; 
+  $wp_query->is_single = false; 
+  $wp_query->is_attachment = false;
+  $wp_query->is_archive = false; 
+  $wp_query->is_category = false;
+  $wp_query->is_tag = false; 
+  $wp_query->is_tax = false;
+  $wp_query->is_author = false;
+  $wp_query->is_date = false;
+  $wp_query->is_year = false;
+  $wp_query->is_month = false;
+  $wp_query->is_day = false;
+  $wp_query->is_time = false;
+  $wp_query->is_search = false;
+  $wp_query->is_feed = false;
+  $wp_query->is_comment_feed = false;
+  $wp_query->is_trackback = false;
+  $wp_query->is_home = false;
+  $wp_query->is_embed = false;
+  $wp_query->is_404 = false; 
+  $wp_query->is_paged = false;
+  $wp_query->is_admin = false; 
+  $wp_query->is_preview = false; 
+  $wp_query->is_robots = false; 
+  $wp_query->is_posts_page = false;
+  $wp_query->is_post_type_archive = false;
+  $GLOBALS['wp_query'] = $wp_query;
+  $wp->register_globals();
+}
+
 /*
  * Function to get active page number used for pagination
  * @return int current page number for pagination
