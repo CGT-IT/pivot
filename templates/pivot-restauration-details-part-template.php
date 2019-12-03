ce2326892f270e995046b692c1090ecd71a61530
mdegembe <maximedeg@gmail.com>
2019-12-03 10:36:58 +0100

    remove duplicate call to same function

--- templates/pivot-restauration-details-part-template.php
+++ templates/pivot-restauration-details-part-template.php
@@ -1,8 +1,9 @@
 <?php $offre = $args; ?>
 
 <div class="offers-area-col <?php print ($offre->map==1)?'col-12':'col-xl-3 col-lg-4 col-md-6 col-sm-6 col-xs-12';?>  mb-3">
-  
-  <?php $url = get_bloginfo('wpurl').'/'.$offre->path.'/'.$offre->attributes()->codeCgt->__toString().'&type='.$offre->typeOffre->attributes()->idTypeOffre->__toString(); ?>
+  <?php $codeCGT = $offre->attributes()->codeCgt->__toString(); ?>
+  <?php $lang = substr(get_locale(), 0, 2 ); ?>
+  <?php $url = get_bloginfo('wpurl').(($lang=='fr')?'':'/'.$lang).'/'.$offre->path.'/'.$codeCGT.'&type='.$offre->typeOffre->attributes()->idTypeOffre->__toString(); ?>
   <div class="card text-left pivot-offer">
     <div class="container-img">
       <img class="pivot-img card-img-top zoom pivot-img-list" src="https://pivotweb.tourismewallonie.be/PivotWeb-3.1/img/<?php print $offre->attributes()->codeCgt->__toString() ;?>;w=428;h=284"/>
@@ -40,7 +41,7 @@
       </p>
       <a target="_blank" class="text-dark stretched-link" title="<?php echo __('Link to', 'pivot') .' '. $offerTitle; ?>" href="<?php print $url; ?>"></a>
       <span class="pivot-id-type-offre d-none item"><?php print $offre->typeOffre->attributes()->idTypeOffre->__toString(); ?></span>
-      <span class="pivot-code-cgt d-none item"><?php print $offre->attributes()->codeCgt->__toString(); ?></span>
+      <span class="pivot-code-cgt d-none item"><?php print $codeCGT; ?></span>
       <span class="pivot-latitude d-none item"><?php print $offre->adresse1->latitude->__toString(); ?></span>
       <span class="pivot-longitude d-none item"><?php print $offre->adresse1->longitude->__toString(); ?></span>
     </div>
