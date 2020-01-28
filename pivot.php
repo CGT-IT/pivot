19f3b95ee30f97659507a93344f377106c61840d
mdegembe <maximedeg@gmail.com>
2020-01-28 14:08:14 +0100

    change organisme details-part template

--- pivot.php
+++ pivot.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Pivot
  * Description: Un plugin pour l'affichage et la recherche (via webservice) des offres touristiques disponibles dans la DB Pivot
- * Version: 1.3.3
+ * Version: 1.3.4
  * Author: Maxime Degembe
  * License: GPL2
  * Text Domain: pivot
--- templates/pivot-organisme-details-part-template.php
+++ templates/pivot-organisme-details-part-template.php
@@ -1,11 +1,18 @@
 <?php $offre = $args; ?>
 
-<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-3">
-
-  <div class="card text-center">
-    <h5 class="card-header"><?php print _get_urn_value($offre, 'urn:fld:nomofr'); ?></h5>
-    <div class="card-body">
-      <p class="card-text">
+<div class="offers-area-col <?php print _set_nb_col($offre->map, $offre->nb_per_row).'nb-col-'.$offre->nb_per_row; ?> mb-3">
+  <?php $codeCGT = $offre->attributes()->codeCgt->__toString(); ?>
+  <?php $lang = substr(get_locale(), 0, 2 ); ?>
+  <?php $url = get_bloginfo('wpurl').(($lang=='fr')?'':'/'.$lang).'/'.$offre->path.'/'.$codeCGT.'&type='.$offre->typeOffre->attributes()->idTypeOffre->__toString(); ?>
+ 
+  <div class="card text-left pivot-offer">
+    <div class="card-orientation <?php print ($offre->map!=1||wp_is_mobile()==1)?'':'card-horizontal';?>">
+      <div class="container-img <?php print ($offre->map!=1||wp_is_mobile()==1)?'':'col-5 p-0 my-auto';?>">
+        <img class="pivot-img card-img-top zoom pivot-img-list" src="https://pivotweb.tourismewallonie.be/PivotWeb-3.1/img/<?php print $codeCGT;?>;w=444;h=296"/>
+      </div>
+      <?php $offerTitle = _get_urn_value($offre, 'urn:fld:nomofr'); ?>
+      <h6 class="title-header pt-1 pb-1 mb-1 card-header" <?php print ($offre->map!=1||wp_is_mobile()==1)?'':'style="display:none"';?>><?php print $offerTitle; ?></h6>
+      <div class="card-body pt-1 pb-2 <?php print ($offre->map!=1||wp_is_mobile()==1)?'':'col-7 pt-2 pb-0';?>">
         <i class="fas fa-map-marker-alt"></i>
         <span>
           <?php print $offre->adresse1->rue->__toString(); ?>, 
@@ -14,13 +21,13 @@
         <br>
         <?php print $offre->adresse1->cp; ?> 
         <?php print $offre->adresse1->localite->value->__toString(); ?>
-      </p>
-      <p class="card-text" style="height: 25px;">
-        <?php if(_get_urn_value($offre, 'urn:fld:urlweb')): ?>
-          <img class="pivot-picto" src="https://pivotweb.tourismewallonie.be:443/PivotWeb-3.1/img/urn:fld:urlweb;h=16"/>
-          <a target="_blank" href="<?php print _get_urn_value($offre, 'urn:fld:urlweb'); ?>"><?php esc_html_e('Website', 'pivot');?></a>
-        <?php endif; ?>
-      </p>
+        <p class="card-text">
+          <?php if(_get_urn_value($offre, 'urn:fld:urlweb')): ?>
+            <img class="pivot-picto" src="https://pivotweb.tourismewallonie.be:443/PivotWeb-3.1/img/urn:fld:urlweb;h=16"/>
+            <a target="_blank" href="<?php print _get_urn_value($offre, 'urn:fld:urlweb'); ?>"><?php esc_html_e('Website', 'pivot');?></a>
+          <?php endif; ?>
+        </p>
+      </div>
     </div>
   </div>
       
