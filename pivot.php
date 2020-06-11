7b8cc96a6127b7bf8e6c134ed5c5bdd12542af60
mdegembe <maximedeg@gmail.com>
2020-06-11 17:37:38 +0200

    up version

--- pivot.php
+++ pivot.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Pivot
  * Description: Un plugin pour l'affichage et la recherche (via webservice) des offres touristiques disponibles dans la DB Pivot
- * Version: 1.6.1
+ * Version: 1.6.2
  * Author: Maxime Degembe
  * License: GPL2
  * Text Domain: pivot
